76808da5b58bbc3d4d3bd95692fc8501b0f2e234
jnannie <jnannie@student.21-school.ru>
2020-11-13 06:47:26 +0300

    philo and monitor threads

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,90 +6,202 @@
 /*   By: jnannie <jnannie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 22:28:48 by jnannie           #+#    #+#             */
-/*   Updated: 2020/11/04 01:08:24 by jnannie          ###   ########.fr       */
+/*   Updated: 2020/11/13 06:29:25 by jnannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include "philo_one.h"
 
-#include <stdio.h>
+t_data			g_data;
 
-static int	read_settings(t_data *data, int argc,
-									char **argv)
+static int	ph_time(void)
+{
+	struct timeval	tv;
+	long			current_time;
+
+	gettimeofday(&tv, NULL);
+	current_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return ((int)(current_time - g_data.start_time));
+}
+
+static int	read_settings(int argc, char **argv)
 {
 	if (argc < 5 || argc > 6)
 		return (error_wrong_number_arguments());
-	memset(data, '\0', sizeof(*data));
-	data->number_of_philos = ft_atoi(argv[1]);
-	data->time_to_die = ft_atoi(argv[2]);
-	data->time_to_eat = ft_atoi(argv[3]);
-	data->time_to_sleep = ft_atoi(argv[4]);
+	g_data.number_of_philos = ft_atoi(argv[1]);
+	g_data.time_to_die = ft_atoi(argv[2]);
+	g_data.time_to_eat = ft_atoi(argv[3]);
+	g_data.time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		data->number_to_eat = ft_atoi(argv[5]);
+		g_data.number_to_eat = ft_atoi(argv[5]);
 	return (0);
 }
 
-static int	init_philosophers(t_philosopher **philos, int number_of_philos)
+static int	init_philosophers(void)
 {
-	int		i;
+	int				i;
+	struct timeval	tv;
 
-	if (!(*philos = ft_calloc(number_of_philos, sizeof(t_philosopher))))
+	gettimeofday(&tv, NULL);
+	g_data.start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	if (!(g_data.philos = ft_calloc(g_data.number_of_philos, sizeof(t_philosopher))))
 		return (fatal_error());
 	i = 0;
-	while (i < number_of_philos)
+	while (i < g_data.number_of_philos)
 	{
+		g_data.philos[i].last_eat_time = g_data.start_time;
+		g_data.philos[i].i = i + 1;
+		pthread_mutex_init(&(g_data.philos)[i].left_fork, NULL);
+		pthread_mutex_init(&(g_data.philos)[i].eat_time_mutex, NULL);
 		if (i > 0)
-			(*philos)[i].right_fork = (*philos)[i - 1].left_fork;
-		pthread_mutex_init((*philos)[i].left_fork, NULL);
+			(g_data.philos)[i].right_fork = (g_data.philos)[i - 1].left_fork;
 		i++;
 	}
-	(*philos)[0].right_fork = (*philos)[number_of_philos - 1].left_fork;
+	(g_data.philos)[0].right_fork = (g_data.philos)[g_data.number_of_philos - 1].left_fork;
 	return (0);
 }
 
-static void	change_state(t_philosopher *philo, int state)
+static void	change_state(char *state, pthread_mutex_t *output_mutex, int i)
 {
+	// struct timeval	tv;
+	int				current_time;
+
+	pthread_mutex_lock(output_mutex);
+	if (!g_data.some_philo_is_dead)
+	{
+		if (ft_strcmp(state, PH_DIED) == 0)
+			g_data.some_philo_is_dead = 1;
+		// gettimeofday(&tv, NULL);
+		current_time = ph_time();
+		ft_putnbr_fd(current_time, 1);
+		ft_putstr_fd(" philosopher ", 1);
+		ft_putnbr_fd(i, 1);
+		ft_putstr_fd(state, 1);
+	}
+	pthread_mutex_unlock(output_mutex);
 	return ;
 }
 
-static void	*philo_lifecycle(void *data, t_philosopher *philo)
+static void	take_forks(t_philosopher *philo)
 {
-	pthread_mutex_lock(((t_philosopher *)philo)->right_fork);
-	change_state(philo, PH_TOOK_RIGHT_FORK);
-	pthread_mutex_lock(((t_philosopher *)philo)->left_fork);
-	change_state(philo, PH_TOOK_LEFT_FORK);
-	change_state(philo, PH_EATING);
-	usleep(((t_data *)data)->time_to_eat);
+	if (philo->i % 2)
+		pthread_mutex_lock(&philo->left_fork);
+	else
+		pthread_mutex_lock(&philo->right_fork);
+	change_state(PH_FORK_TAKEN, &g_data.output_mutex, philo->i);
+	if (!(philo->i % 2))
+		pthread_mutex_lock(&philo->left_fork);
+	else
+		pthread_mutex_lock(&philo->right_fork);
+	change_state(PH_FORK_TAKEN, &g_data.output_mutex, philo->i);
 }
 
-static int	run_threads(t_data *data)
+static void	put_forks_back(t_philosopher *philo)
+{
+	if (philo->i % 2)
+		pthread_mutex_unlock(&philo->left_fork);
+	else
+		pthread_mutex_unlock(&philo->right_fork);
+	if (!(philo->i % 2))
+		pthread_mutex_unlock(&philo->left_fork);
+	else
+		pthread_mutex_unlock(&philo->right_fork);
+}
+
+static void	*philo_lifecycle(void *philo)
+{
+	// struct timeval	tv;
+
+	while (!g_data.some_philo_is_dead)
+	{
+		take_forks((t_philosopher *)philo);
+		change_state(PH_EATING, &g_data.output_mutex, ((t_philosopher *)philo)->i);
+		usleep(g_data.time_to_eat * 1000);
+
+		// pthread_mutex_lock(&g_data.tv_mutex);
+		// gettimeofday(&tv, NULL);
+		// ((t_philosopher *)philo)->eat_time = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+		pthread_mutex_lock(&((t_philosopher *)philo)->eat_time_mutex);
+		((t_philosopher *)philo)->last_eat_time = ph_time();
+		pthread_mutex_unlock(&((t_philosopher *)philo)->eat_time_mutex);
+		// pthread_mutex_unlock(&g_data.tv_mutex);
+
+		put_forks_back((t_philosopher *)philo);
+		change_state(PH_SLEEPING, &g_data.output_mutex, ((t_philosopher *)philo)->i);
+		usleep(g_data.time_to_sleep * 1000);
+		change_state(PH_THINKING, &g_data.output_mutex, ((t_philosopher *)philo)->i);
+	}
+	return (0);
+}
+
+static void	*monitoring(void *philo)
+{
+	// struct timeval	tv;
+	long			current_time;
+	long			last_eat_time;
+
+	while (1)
+	{
+		pthread_mutex_lock(&((t_philosopher *)philo)->eat_time_mutex);
+		last_eat_time = ((t_philosopher *)philo)->last_eat_time;
+		pthread_mutex_unlock(&((t_philosopher *)philo)->eat_time_mutex);
+		current_time = ph_time();
+		if ((current_time - last_eat_time) > (g_data.time_to_die))
+		{
+			// ((t_philosopher *)philo)->is_dead = 1;
+			pthread_mutex_lock(&g_data.check_dead_philo_mutex);
+			if (!g_data.some_philo_is_dead)
+			{
+				change_state(PH_DIED, &g_data.output_mutex, ((t_philosopher *)philo)->i);
+				// g_data.some_philo_is_dead = 1;
+			}
+			pthread_mutex_unlock(&g_data.check_dead_philo_mutex);
+			break ;
+		}
+		usleep(MONITORING_DELAY);
+	}
+	return (0);
+}
+
+static void	run_threads(void)
 {
 	int		i;
 
 	i = 0;
-	while (i < data->number_of_philos)
+	while (i < g_data.number_of_philos)
 	{
-		pthread_create(&data->philos[i].thread, NULL, philo_lifecycle,
-						data);
+		pthread_create(&g_data.philos[i].philo_thread, NULL, philo_lifecycle, g_data.philos + i);
+		pthread_create(&g_data.philos[i].monitor_thread, NULL, monitoring, g_data.philos + i);
+		i++;
+	}
+}
+
+static void	join_threads(void)
+{
+	int		i;
+
+	i = 0;
+	while (i < g_data.number_of_philos)
+	{
+		pthread_join(g_data.philos[i].philo_thread, NULL);
+		pthread_join(g_data.philos[i].monitor_thread, NULL);
 		i++;
 	}
 }
 
 int			main(int argc, char **argv)
 {
-	t_data			data;
-	// t_philosopher	*philos;
-	char			*str;
-
-	if (read_settings(&data, argc, argv) == -1 ||
-		init_philosophers(&data.philos, data.number_of_philos) == -1)
+	if (read_settings(argc, argv) == -1 ||
+		init_philosophers() == -1)
 		return (1);
-	run_threads(&data);
-	// create threads with eating, sleeping and thinking
-	// create one companion statuses monitoring thread
-	// profit
+	pthread_mutex_init(&g_data.output_mutex, NULL);
+	pthread_mutex_init(&g_data.check_dead_philo_mutex, NULL);
+	run_threads();
+	join_threads();
 	return (0);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -6,20 +6,35 @@
 /*   By: jnannie <jnannie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 22:03:49 by jnannie           #+#    #+#             */
-/*   Updated: 2020/11/04 00:50:36 by jnannie          ###   ########.fr       */
+/*   Updated: 2020/11/13 06:22:59 by jnannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
+# include <pthread.h>
 # include <stddef.h>
 
-# define PH_TOOK_RIGHT_FORK 1
-# define PH_TOOK_LEFT_FORK 2
-# define PH_EATING 3
-# define PH_SLEEPING 4
-# define PH_THINKING 5
+# define PH_FORK_TAKEN " has taken a fork\n"
+# define PH_EATING " is eating\n"
+# define PH_SLEEPING " is sleeping\n"
+# define PH_THINKING " is thinking\n"
+# define PH_DIED " died\n"
+# define MONITORING_DELAY 1000
+
+typedef struct		s_philosopher
+{
+	pthread_mutex_t	left_fork;
+	pthread_mutex_t	right_fork;
+	pthread_mutex_t	eat_time_mutex;
+	pthread_t		philo_thread;
+	pthread_t		monitor_thread;
+	int				state;
+	int				i;
+	long			last_eat_time;
+	int				is_dead;
+}					t_philosopher;
 
 typedef struct		s_data
 {
@@ -29,20 +44,20 @@ typedef struct		s_data
 	int				time_to_sleep;
 	int				number_to_eat;
 	t_philosopher	*philos;
+	pthread_mutex_t	output_mutex;
+	pthread_mutex_t	check_dead_philo_mutex;
+	int				some_philo_is_dead;
+	long			start_time;
 }					t_data;
 
-typedef struct		s_philosopher
-{
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-	pthread_t		thread;
-	int				state;
-}					t_philosopher;
+extern t_data		g_data;
 
 void				*ft_calloc(size_t nmemb, size_t size);
 int					ft_atoi(const char *nptr);
 size_t				ft_strlen(const char *s);
 void				ft_putstr_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+int					ft_strcmp(char *s1, char *s2);
 
 int					error_wrong_number_arguments(void);
 int					fatal_error(void);
