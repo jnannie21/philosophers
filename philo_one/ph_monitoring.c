1d4859d7c8c694824c076086a0dc2dcb382e5b24
jnannie <jnannie@student.21-school.ru>
2020-12-02 11:24:14 +0300

    timing in monitoring corrected back in all philos

--- philo_one/ph_monitoring.c
+++ philo_one/ph_monitoring.c
@@ -6,7 +6,7 @@
 /*   By: jnannie <jnannie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 17:45:41 by jnannie           #+#    #+#             */
-/*   Updated: 2020/11/29 13:46:05 by jnannie          ###   ########.fr       */
+/*   Updated: 2020/12/02 10:58:43 by jnannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*monitoring(void *philo)
 		pthread_mutex_lock(&((t_philosopher *)philo)->eat_time_mutex);
 		last_eat_time = ((t_philosopher *)philo)->last_eat_time;
 		current_time = ph_time();
-		if ((current_time - last_eat_time) >= (g_data.time_to_die))
+		if ((current_time - last_eat_time) > (g_data.time_to_die))
 			change_state(PH_DIED, (t_philosopher *)philo);
 		pthread_mutex_unlock(&((t_philosopher *)philo)->eat_time_mutex);
 		usleep(PH_MONITORING_DELAY);
--- philo_three/ph_monitoring.c
+++ philo_three/ph_monitoring.c
@@ -6,7 +6,7 @@
 /*   By: jnannie <jnannie@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 17:45:41 by jnannie           #+#    #+#             */
-/*   Updated: 2020/11/29 13:42:02 by jnannie          ###   ########.fr       */
+/*   Updated: 2020/12/02 10:56:28 by jnannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	*monitoring(void *philo)
 		sem_wait(((t_philosopher *)philo)->eat_time_sem);
 		last_eat_time = ((t_philosopher *)philo)->last_eat_time;
 		current_time = ph_time();
-		if ((current_time - last_eat_time) >= (g_data.time_to_die))
+		if ((current_time - last_eat_time) > (g_data.time_to_die))
 			change_state(PH_DIED, (t_philosopher *)philo);
 		sem_post(((t_philosopher *)philo)->eat_time_sem);
 		usleep(PH_MONITORING_DELAY);
